4f8cd9761a367a9efcb337a62913fbadfa161455
denistet2 <76530306+denistet2@users.noreply.github.com>
2022-07-08 13:59:13 +0300

    строки 1

---
+++ strings1.py
@@ -0,0 +1,15 @@
+_str = ('автомобили')
+print(_str[2])
+print(_str[-2])
+print(_str[:5])
+print(_str[:-2])
+for i in range(0, len(_str), 2):
+    print(_str[i])
+for i in range(1, len(_str), 2):
+    print(_str[i])
+print(_str[::-1])
+print(_str[0:-10:-1])
+print(len(_str))
+
+
+
